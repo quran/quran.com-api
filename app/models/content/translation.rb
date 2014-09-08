b4b0abf9202fee6f4d89af91c2f33dad41fbe6f6
Mohamed El Mahallawy <mmahalwy@gmail.com>
2014-09-08 19:46:03 -0400

    allow for search

--- app/models/content/translation.rb
+++ app/models/content/translation.rb
@@ -15,83 +15,55 @@ class Content::Translation < ActiveRecord::Base
       indexes :text
     end
 
-    
-    def self.importing (options = {})
+
+    def self.import(options = {})
         transform = lambda do |a|
             {index: {_id: "#{a.resource_id},#{a.ayah_key}", _parent: a.ayah_key, data: a.__elasticsearch__.as_indexed_json}} 
         end
         options = {transform: transform}.merge(options)
-        self.import options 
+        self.importing options 
     end
 
 
-    def self.searching ( ayat = [] )
-        msearch_body = [ {
-            search: {
-#            highlight: {
-#                fields: {
-#                    text: {
-#                        type: 'fvh',
-#                        number_of_fragments: 1,
-#                        fragment_size: 1024
-#                    }
-#                },
-#                tags_schema => 'styled'
-#            },
-                query: {
-                    bool: {
-                        must: [ {
-                            term: {
-                                _parent: {
-                                    value: "100:5"
+    def self.matched_children (query, ayat = [] )
+        msearch_body = []
+        ayat.each do |ayah_key|
+            ayah = {
+                search: {
+    #            highlight: {
+    #                fields: {
+    #                    text: {
+    #                        type: 'fvh',
+    #                        number_of_fragments: 1,
+    #                        fragment_size: 1024
+    #                    }
+    #                },
+    #                tags_schema => 'styled'
+    #            },
+                    query: {
+                        bool: {
+                            must: [ {
+                                term: {
+                                    _parent: {
+                                        value: ayah_key
+                                    }
                                 }
-                            }
-                        }, {
-                            match: {
-                                text: {
-                                    query: "armikut",
-                                    operator: 'or',
-                                    minimum_should_match: '3<62%'
+                            }, {
+                                match: {
+                                    text: {
+                                        query: query,
+                                        operator: 'or',
+                                        minimum_should_match: '3<62%'
+                                    }
                                 }
-                            }
-                        } ]
+                            } ]
+                        }
                     }
                 }
             }
-        }, {
-            search: {
-#            highlight: {
-#                fields: {
-#                    text: {
-#                        type: 'fvh',
-#                        number_of_fragments: 1,
-#                        fragment_size: 1024
-#                    }
-#                },
-#                tags_schema => 'styled'
-#            },
-                query: {
-                    bool: {
-                        must: [ {
-                            term: {
-                                _parent: {
-                                    value: "100:6"
-                                }
-                            }
-                        }, {
-                            match: {
-                                text: {
-                                    query: "armikut",
-                                    operator: 'or',
-                                    minimum_should_match: '3<62%'
-                                }
-                            }
-                        } ]
-                    }
-                }
-            }
+            msearch_body.push(ayah)
+        end
 
-        } ]
         msearch_query = {
             index: 'quran2',
             body: msearch_body
