e85640cc3ee3b83c1425ebb14a665f412e844d02
Mohamed El Mahallawy <mmahalwy@gmail.com>
2014-09-11 14:49:00 -0400

    added setup index for mappings problems

--- app/models/concerns/searchable.rb
+++ app/models/concerns/searchable.rb
@@ -1,19 +1,47 @@
 module Searchable
   extend ActiveSupport::Concern
 
+
+  def self.setup_index
+    models = [Content::Translation, Content::Transliteration, Quran::Ayah]
+    mappings = Hash.new
+
+    models.each do |model|
+      mappings = mappings.merge(model.mappings.to_hash)
+    end
+    
+      models.first.__elasticsearch__.client.indices.create \
+        index: "quran",
+        body: { settings: models.first.settings.to_hash, mappings: mappings }
+    # end
+  end 
+
   included do
     include Elasticsearch::Model
     include Elasticsearch::Model::Callbacks
+    Kaminari::Hooks.init
+    Elasticsearch::Model::Response::Response.__send__ :include, Elasticsearch::Model::Response::Pagination::Kaminari
 
     class << self
         alias_method :importing, :import
+        alias_method :searching, :search
     end
 
     self.settings index: { number_of_shards: 1 } do
-        mappings dynamic: 'false' do
+        mappings dynamic: 'strict' do
         end
     end
-    self.index_name 'quran2'
+    self.index_name 'quran'
+    # self.document_type = self.name.split("::").first.downcase
+    
+
+
+    
+
+    # Rails.logger.error Rails.root
+    # YAML.load(File.read(File.expand_path("#{Rails.root}/config/elasticsearch/mappings.yml", __FILE__)))
+    # 
+    
     # mapping do
     #   # ...
     # end
