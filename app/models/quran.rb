2a59880c2dcf2e291e964c6e5b759fd47b4322e5
Nour Sharabash <nour.sharabash@gmail.com>
2014-09-12 08:02:44 -0500

    centralized elasticsearch config automatically applied to index settings (defined in Schema => included in Quran => extended to Quran::Ayah)

--- app/models/quran.rb
+++ app/models/quran.rb
@@ -1,18 +1,4 @@
 module Quran
-
     include Schema
-
-    def self.extended(mod)
-        Rails.logger.error mod
-        mod.include(Elasticsearch::Model)
-        mod.include(Elasticsearch::Model::Callbacks)
-        mod.settings index: { number_of_shards: 1 } do
-            mappings dynamic: 'false' do
-            end
-        end
-        mod.index_name 'quran2'
-    end
-
-
     Schema.schema_name = 'quran'
 end
--- app/models/schema.rb
+++ app/models/schema.rb
@@ -17,4 +17,20 @@ module Schema
     def self.schema_name=(schema_name)
         @@schema_name["#{schema_name}"] = schema_name
     end
+ 
+    def self.included( mod )
+        def mod.extended( mod )
+            Rails.logger.debug( 'RAILS ROOT '+ File.dirname( __FILE__ ) )
+            settings = YAML.load_file( "config/elasticsearch/settings.yml" )
+            #Rails.logger.debug( settings.to_hash )
+            Rails.logger.error mod
+            mod.include(Elasticsearch::Model)
+            mod.include(Elasticsearch::Model::Callbacks)
+            mod.settings settings do
+                mappings dynamic: 'false' do
+                end
+            end
+            mod.index_name 'quran2'
+        end
+    end
 end
