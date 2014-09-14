2c12049477bd8c7cca2285990c8956c7e550a055
Nour Sharabash <nour.sharabash@gmail.com>
2014-09-14 09:10:35 -0500

    cleaned up whitespace and tabbing in searchable.rb

--- app/models/concerns/searchable.rb
+++ app/models/concerns/searchable.rb
@@ -7,15 +7,10 @@ module Searchable
     settings = YAML.load( File.read( File.expand_path( "#{Rails.root}/config/elasticsearch/settings.yml", __FILE__ ) ) )
     mappings = YAML.load( File.read( File.expand_path( "#{Rails.root}/config/elasticsearch/mappings.yml", __FILE__ ) ) )
 
-#    models.each do |model|
-#      mappings = mappings.merge(model.mappings.to_hash)
-#    end
-    
-      models.first.__elasticsearch__.client.indices.create \
-        index: "quran",
-        body: { settings: settings, mappings: mappings }
-    # end
-  end 
+    models.first.__elasticsearch__.client.indices.create \
+      index: "quran",
+      body: { settings: settings, mappings: mappings }
+  end
 
 
   # When this module is included, this callback function is called
@@ -28,28 +23,10 @@ module Searchable
     Elasticsearch::Model::Response::Response.__send__ :include, Elasticsearch::Model::Response::Pagination::Kaminari
 
     class << self
-        alias_method :importing, :import
-        alias_method :searching, :search
+      alias_method :importing, :import
+      alias_method :searching, :search
     end
 
-    # YAML.load(File.read(File.expand_path("#{Rails.root}/config/elasticsearch/mappings.yml", __FILE__)))
-
-#    self.settings es_settings do
-#        mappings dynamic: 'strict' do
-#        end
-#    end
-    
     self.index_name 'quran'
-    
-    
-
-
-    
-
-    # Rails.logger.error Rails.root
-    # YAML.load(File.read(File.expand_path("#{Rails.root}/config/elasticsearch/mappings.yml", __FILE__)))
-    # 
-    
-
   end
 end
