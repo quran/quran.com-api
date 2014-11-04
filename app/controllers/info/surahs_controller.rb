2e74b28a09a28bd3785cb2c2b90eaa1213d50980
Mohamed El Mahallawy <mmahalwy@gmail.com>
2014-11-03 23:36:48 -0800

    Deleted comments

--- app/controllers/info/surahs_controller.rb
+++ app/controllers/info/surahs_controller.rb
@@ -1,8 +1,7 @@
 class Info::SurahsController < ApplicationController
+  
   def index
-
-    @results = Quran::Surah
-    .order("quran.surah.surah_id")
+    @results = Quran::Surah.order("quran.surah.surah_id")
   end
   def show
     @surah = Quran::Surah.find(params[:id])
@@ -11,17 +10,3 @@ end
 
 
 
-
- # qq|
- #            select s.surah_id id
- #                 , s.ayat
- #                 , s.bismillah_pre
- #                 , s.revelation_order
- #                 , s.revelation_place
- #                 , s.page
- #                 , s.name_complex
- #                 , s.name_simple
- #                 , s.name_english
- #                 , s.name_arabic
- #              from quran.surah s |.( ( not $vars{surah} ) ? '' : 'where surah_id = ?' ).qq|
- #             order by s.surah_id
