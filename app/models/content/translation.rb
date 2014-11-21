deb40a2df705f3b8c721804eb32bbc09c007b6be
Nour Sharabash <nour.sharabash@gmail.com>
2014-11-21 08:36:20 -0600

    experimenting with english analyzer

--- app/models/content/translation.rb
+++ app/models/content/translation.rb
@@ -7,18 +7,22 @@ class Content::Translation < ActiveRecord::Base
 
     belongs_to :resource, class_name: 'Content::Resource'
     belongs_to :ayah, class_name: 'Quran::Ayah'
+    scope :ordered,     ->              { joins( 'join quran.ayah on translation.ayah_key = ayah.ayah_key' ).order( 'translation.resource_id asc, ayah.surah_id asc, ayah.ayah_num asc' ) }
+    scope :resource_id, ->(id_resource) { ordered.where( resource_id: id_resource ) }
 
-    def self.import(options = {})
-        transform = lambda do |a|
-            { index: {
-                    _id: "#{a.resource_id},#{a.ayah_key}",
-                _parent: a.ayah_key,
-                   data: a.__elasticsearch__.as_indexed_json.merge( { 'resource' => a.resource.__elasticsearch__.as_indexed_json, 'language' => a.resource.language.__elasticsearch__.as_indexed_json, 'source' => a.resource.source.__elasticsearch__.as_indexed_json, 'author' => a.resource.author.__elasticsearch__.as_indexed_json } )
-            } }
+    def self.import( options = {} )
+        Content::Translation.connection.cache do
+            transform = lambda do |a|
+                { index: {
+                        _id: "#{a.resource_id},#{a.ayah_key}",
+                    _parent: a.ayah_key,
+                       data: a.__elasticsearch__.as_indexed_json.merge( { 'resource' => a.resource.__elasticsearch__.as_indexed_json, 'language' => a.resource.language.__elasticsearch__.as_indexed_json, 'source' => a.resource.source.__elasticsearch__.as_indexed_json, 'author' => a.resource.author.__elasticsearch__.as_indexed_json } )
+                } }
+            end
+
+            options = { transform: transform, batch_size: 6236 }.merge( options )
+            self.importing options
         end
-
-        options = { transform: transform, batch_size: 6236 }.merge(options)
-        self.importing options 
     end
 end
 # notes:
