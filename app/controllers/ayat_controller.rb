a65d11b707c4489d906dd696f2dc9b8af598258f
Mohamed El Mahallawy <mmahalwy@gmail.com>
2015-02-04 22:39:13 -0800

    Added ayah text

--- app/controllers/ayat_controller.rb
+++ app/controllers/ayat_controller.rb
@@ -36,16 +36,19 @@ class AyatController < ApplicationController
         # keys = Quran::Ayah.fetch_ayahs(params[:surah_id], range.first, range.last)
 
         # Keys for the ayahs
-        keys = Quran::Ayah.fetch_ayahs(params[:surah_id], range.first, range.last).map{|k| k.ayah_key}
+        ayahs = Quran::Ayah.fetch_ayahs(params[:surah_id], range.first, range.last)
+
+        keys = ayahs.map{|k| k.ayah_key}
 
         # For each key, need to setup the hash
-        keys.each do |ayah_key|
-            @results["#{ayah_key}".to_sym] = Hash.new
-            @results["#{ayah_key}".to_sym][:ayah] = ayah_key.split(":").last.to_i
-            @results["#{ayah_key}".to_sym][:surah_id] = params[:surah_id].to_i
-            @results["#{ayah_key}".to_sym][:content] = Array.new
-            @results["#{ayah_key}".to_sym][:audio] = Hash.new
-            @results["#{ayah_key}".to_sym][:quran] = Array.new
+        ayahs.each do |ayah|
+            @results["#{ayah.ayah_key}".to_sym] = Hash.new
+            @results["#{ayah.ayah_key}".to_sym][:ayah] = ayah.ayah_key.split(":").last.to_i
+            @results["#{ayah.ayah_key}".to_sym][:surah_id] = ayah.surah_id.to_i
+            @results["#{ayah.ayah_key}".to_sym][:text] = ayah.text
+            @results["#{ayah.ayah_key}".to_sym][:content] = Array.new
+            @results["#{ayah.ayah_key}".to_sym][:audio] = Hash.new
+            @results["#{ayah.ayah_key}".to_sym][:quran] = Array.new
         end
 
         # cardinalities will be used to determine the kind of rendering to fetch
