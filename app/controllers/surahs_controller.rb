577da26553d27541af6fb2f420fe47ddbd8f2e5f
mmahalwy <mmahalwy@gmail.com>
2015-06-25 22:36:01 -0700

    No whitespace

--- app/controllers/surahs_controller.rb
+++ app/controllers/surahs_controller.rb
@@ -6,13 +6,11 @@ class SurahsController < ApplicationController
     @results = Rails.cache.fetch('surahs', expires_in: 3.days) do
       Quran::Surah.order("quran.surah.surah_id")
     end
-
   end
 
   def show
     @surah = Rails.cache.fetch("surahs/#{params[:id]}", expires_in: 3.days) do
       Quran::Surah.find(params[:id])
     end
-
   end
 end
