24450417dd3bde5640794aa0b391be92669c1d39
Mohamed El Mahallawy <mmahalwy@gmail.com>
2016-04-03 01:07:11 -0700

    Elasticsearch initializer

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,11 +1,5 @@
 module Search
-  if Rails.env.production?
-    @@client = Elasticsearch::Client.new host: ENV['ELASTICSEARCH_PORT_9200_TCP_ADDR']
-  else
-    @@client = Elasticsearch::Client.new  # trace: true, log: true;
-  end
-
   def self.client
-    @@client
+    Elasticsearch::Model.client
   end
 end
