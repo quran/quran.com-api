84e9f803950581efa91cb34f2aabb66b5c518d80
mmahalwy <mohamed@peek.com>
2016-07-10 00:14:26 -0400

    Add Sentry errors

---
+++ config/initializers/sentry.rb
@@ -0,0 +1,4 @@
+Raven.configure do |config|
+  config.dsn = 'https://27c82db87fe5458e80fb1f673b28d135:3984ba65fa3540d18245ee517bcd63f0@app.getsentry.com/86381'
+  config.environments = ['staging', 'production']
+end
