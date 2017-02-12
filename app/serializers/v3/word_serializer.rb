52e8922eca6e75601645639f5db2b8578e2ef602
Mohamed El Mahallawy <mmahalwy@gmail.com>
2017-02-11 23:56:55 -0800

    Add Page Number to Word Serializer

--- app/serializers/v3/word_serializer.rb
+++ app/serializers/v3/word_serializer.rb
@@ -19,9 +19,19 @@
 #
 
 class V3::WordSerializer < V3::ApplicationSerializer
-  attributes :id, :position, :text_madani, :text_indopak, :text_simple, :verse_key, :class_name, :line_number, :code, :char_type
+  attributes :id,
+             :position,
+             :text_madani,
+             :text_indopak,
+             :text_simple,
+             :verse_key,
+             :class_name,
+             :line_number,
+             :page_number,
+             :code,
+             :char_type
 
-  has_one  :audio, serializer: V3::AudioFileSerializer
+  has_one :audio, serializer: V3::AudioFileSerializer
 
   has_one :translation do
     object.translations.filter_by_language_or_default scope[:translations]
