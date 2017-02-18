69079e480bb15cce41bdd891a702ee1f5fe9eeb0
Naveed Ahmad <naveedahmada036@gmail.com>
2017-02-19 03:31:40 +0500

    added missing query dsl file

---
+++ lib/search/query.rb
@@ -0,0 +1,22 @@
+module Search
+  class Query
+    attr_reader :query
+    REGEXP = Regexp.union(%w_+ - = & | > < ! ( ) { } [ ] ^ " ~ * ? : \ /_)
+
+    def initialize(query)
+      @query = query.to_s.force_encoding('ASCII-8BIT').force_encoding('UTF-8').gsub(REGEXP) { |m| "\\#{m}" }
+    end
+
+    def is_arabic?
+      @query.arabic?
+    end
+
+    def query
+      @query
+    end
+
+    def language_match
+      @query.prose
+    end
+  end
+end
