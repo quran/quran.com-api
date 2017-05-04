856e07853e04d72f883ea2e453760e8ffe38987a
Naveed Ahmad <naveedahmada036@gmail.com>
2017-05-04 12:31:53 +0500

    added tafsir endpoint

--- app/controllers/v3/tafsirs_controller.rb
+++ app/controllers/v3/tafsirs_controller.rb
@@ -1,9 +1,33 @@
 class V3::TafsirsController < ApplicationController
+  before_action :find_verse
+  
   def index
-    chapter = Chapter.find(params[:chapter_id])
-    verse   = chapter.verses.find(params[:verse_id])
-    tafsirs = verse.tafsirs.where(resource_content_id: params[:tafsirs])
-
+    tafsirs = @verse.tafsirs
+    
+    if(filter = tafirs_filter).present?
+      tafsirs = tafsirs.where(resource_content_id: filter)
+    end
+    
     render json: tafsirs
+  end
+  
+  def show
+    tafisr = @verse.tafsirs.find(params[:id])
+
+    render json: tafsir
+  end
+  
+  protected
+  def find_verse
+    @verse  = Verse.find(params[:verse_id])
+  end
+  
+  def tafirs_filter
+    if params[:tafsirs].present?
+      tafsirs = params[:tafsirs]
+      params[:tafsirs] = ResourceContent.where(id: tafsirs).or(ResourceContent.where(slug: tafsirs)).pluck(:id)
+    end
+    
+    params[:tafsirs]
   end
 end
