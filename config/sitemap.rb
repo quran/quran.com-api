4a5c1e0b11ee5816cf4d1acafcf121ba27978f1a
Naveed Ahmad <naveedahmada036@gmail.com>
2017-06-09 20:44:32 +0500

    added routes for sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,6 +1,6 @@
 # Set the host name for URL creation
 SitemapGenerator::Sitemap.default_host = "https://quran.com"
-SitemapGenerator::Sitemap.sitemaps_host = "http://static.quran.com/"
+SitemapGenerator::Sitemap.sitemaps_host = "https://quran.com/"
 SitemapGenerator::Sitemap.sitemaps_path = 'sitemaps/'
 
 SitemapGenerator::Sitemap.create do
