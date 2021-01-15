fdd6973da9c9efdaa90593759ab53b86db7bd4f4
Naveed Ahmad <naveedahmada036@gmail.com>
2021-01-16 03:44:16 +0500

    updated db config

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,7 +1,9 @@
-options = if ENV['ELASTICSEARCH_HOST']
-  {host: ENV['ELASTICSEARCH_HOST']}
-else
-  {}  # trace: true, log: true;
-end.merge adapter: :excon
+options = if ENV['ELASTICSEARCH_PORT_9200_TCP_ADDR']
+            {host: ENV['ELASTICSEARCH_PORT_9200_TCP_ADDR']}
+          else
+            {
+                host: '127.0.0.1'
+            }
+          end.merge(adapter: :typhoeus)
 
 Elasticsearch::Model.client = Elasticsearch::Client.new options
