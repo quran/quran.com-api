62abb4745ede8e6744f692e1d55e975fd6668e2c
Naveed Ahmad <naveedahmada036@gmail.com>
2021-04-23 01:04:24 +0500

    added v2_page to graphql

--- app/graphql/types/word_type.rb
+++ app/graphql/types/word_type.rb
@@ -11,13 +11,16 @@ module Types
     field :text_simple, String, null: true
     field :text_imlaei, String, null: true
     field :verse_key, String, null: true
-    field :page_number, Integer, null: true
     field :class_name, String, null: true
     field :line_number, Integer, null: true
     field :code, String, null: true
     field :location, String, null: true
     field :audio_url, String, null: true
     field :char_type_name, String, null: true
+
+    field :page_number, Integer, null: true
+    field :v1_page, Integer, null: true
+    field :v2_page, Integer, null: true
 
     field :translation, Types::WordTranslationType, "word translation in given language. If translation for give language doesn't exist, we'll fallback to English translation", null: true
 
