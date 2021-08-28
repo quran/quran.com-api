e9cd51d80e3b0b84612a6039d27fe63844555fd3
Naveed Ahmad <naveedahmada036@gmail.com>
2021-08-28 05:48:09 +0500

    rollback migration change

--- db/migrate/20210224062456_create_mushaf_pages.rb
+++ db/migrate/20210224062456_create_mushaf_pages.rb
@@ -1,6 +1,6 @@
 class CreateMuhsafPages < ActiveRecord::Migration[6.1]
   def change
-    ActiveRecord::Migration.create_table :mushaf_pages do |t|
+    create_table :mushaf_pages do |t|
       t.integer :page_number, index: true
       t.integer :mushaf_id, index: true
 
