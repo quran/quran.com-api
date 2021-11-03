1dc8f292d0dc1191f02982a5850a4e0df7282a7a
Naveed Ahmad <naveedahmada036@gmail.com>
2021-11-04 00:03:34 +0500

    footnote

--- app/controllers/api/v3/foot_notes_controller.rb
+++ app/controllers/api/v3/foot_notes_controller.rb
@@ -4,7 +4,7 @@ module Api::V3
   class FootNotesController < ApiController
     # GET /foot_notes/1
     def show
-      foot_note = FootNote.find(params[:id])
+      @foot_note = FootNote.find(params[:id])
 
       render
     end
