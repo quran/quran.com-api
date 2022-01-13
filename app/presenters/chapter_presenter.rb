d217e4dc918ea3910c3d3d8a6fca995aeef48125
Naveed Ahmad <naveedahmada036@gmail.com>
2022-01-13 16:57:17 +0500

    fix chapter presenter

--- app/presenters/chapter_presenter.rb
+++ app/presenters/chapter_presenter.rb
@@ -8,19 +8,34 @@ class ChapterPresenter < BasePresenter
     @locale = locale
   end
 
-  def chapters
-    finder.all_with_eager_load(locale: locale)
-  end
-
   def chapter
     strong_memoize :chapter do
-      finder.find_and_eager_load(params[:id].strip, locale: locale)
+      finder.find_and_eager_load(params[:id].strip, locale: locale, include_slugs: include_slugs?)
     end
   end
 
-  protected
+  def include_slugs?
+    include_in_response? params[:slugs]
+  end
 
+  def include_translated_names?
+    include_in_response? params[:translated_names]
+  end
+
+  def include_chapter_info?
+    include_in_response? params[:info]
+  end
+
+  def chapter_info
+    info = ChapterInfo
+             .where(chapter_id: chapter.id)
+             .filter_by_language_or_default(fetch_locale)
+
+    info || raise_404("Surah not found")
+  end
+
+  protected
   def finder
-    ChapterFinder.new
+    ChapterFinder.new(params)
   end
 end
--- app/presenters/qdc/chapter_presenter.rb
+++ app/presenters/qdc/chapter_presenter.rb
@@ -1,48 +1,6 @@
 # frozen_string_literal: true
 
 module Qdc
-  class ChapterPresenter < BasePresenter
-    attr_reader :locale
-
-    def initialize(params, locale)
-      super(params)
-      @locale = locale
-    end
-
-    def chapters
-      finder.all_with_eager_load(locale: locale, include_slugs: include_slugs?)
-    end
-
-    def chapter
-      strong_memoize :chapter do
-        finder.find_and_eager_load(params[:id], locale: locale, include_slugs: include_slugs?)
-      end
-    end
-
-    def include_slugs?
-      include_in_response? params[:slugs]
-    end
-
-    def include_translated_names?
-      include_in_response? params[:translated_names]
-    end
-
-    def include_chapter_info?
-      include_in_response? params[:info]
-    end
-
-    def chapter_info
-      info = ChapterInfo
-               .where(chapter_id: chapter.id)
-               .filter_by_language_or_default(fetch_locale)
-
-      info || raise_404("Surah not found")
-    end
-
-    protected
-
-    def finder
-      ChapterFinder.new
-    end
+  class ChapterPresenter < ::ChapterPresenter
   end
 end
