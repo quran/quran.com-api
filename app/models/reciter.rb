669e65a0a9fba882e801d0b9d2e62fa8aa01378a
Naveed Ahmad <naveedahmada036@gmail.com>
2022-01-23 21:20:17 +0500

    annotate

--- app/models/reciter.rb
+++ app/models/reciter.rb
@@ -5,7 +5,10 @@
 # Table name: reciters
 #
 #  id                :integer          not null, primary key
+#  bio               :text
+#  cover_image       :string
 #  name              :string
+#  profile_picture   :string
 #  recitations_count :integer          default(0)
 #  created_at        :datetime         not null
 #  updated_at        :datetime         not null
