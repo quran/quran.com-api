a396d06ba314d2882433f5eaa5ab233806ec9073
Naveed Ahmad <naveedahmada036@gmail.com>
2022-02-17 23:34:30 +0500

    pagy overflow is extracted in plugin

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -169,7 +169,7 @@ require 'pagy/extras/elasticsearch_rails'
 
 # Overflow extra: Allow for easy handling of overflowing pages
 # See https://ddnexus.github.io/pagy/extras/overflow
-# require 'pagy/extras/overflow'
+require 'pagy/extras/overflow'
 Pagy::DEFAULT[:overflow] = :last_page    # default  (other options: :last_page and :exception)
 
 # Support extra: Extra support for features like: incremental, infinite, auto-scroll pagination
