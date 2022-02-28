b7199bf9bdacb78d670cdd4152e55fb635769471
Naveed Ahmad <naveedahmada036@gmail.com>
2022-02-28 23:28:14 +0500

    fix pagy

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,3 +1,8 @@
+require 'typhoeus'
+
+# https://github.com/typhoeus/typhoeus/pull/582
+Faraday::Adapter.register_middleware typhoeus: :Typhoeus
+
 options = if ENV['ELASTICSEARCH_PORT_9200_TCP_ADDR']
             {host: ENV['ELASTICSEARCH_PORT_9200_TCP_ADDR']}
           else
